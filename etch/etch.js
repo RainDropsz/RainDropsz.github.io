70d6a19d9b2b5c4635373a55eaa4e56a8cd5c239
RainDropsz <38995788+RainDropsz@users.noreply.github.com>
2020-03-29 17:32:30 -0400

    Add files via upload

--- etch/etch.js
+++ etch/etch.js
@@ -1,62 +1,148 @@
-//Draw Grid
-//Number of boxes wide and high
 var nwide = 16, nhght = 16;
 var pctwide, pcthght;
-var i =0, j = 0;
+var i = 0, j = 0;
+var color1 = $("#colorChoice").val();
+var colorText = $("#colorText").val();
+var mode = 0;
+var opacity = 0.1;
 
 buildGrid(16,16);
 
 function buildGrid(nwide, nhght) {
-$("#grid1").detach();
-//Add div #grid1 as grid container
-$("body").append('<div id="grid1"></div>');
 
+          //Remove previous grid
+  $("#grid1").remove();
+  
+          //Add div #grid1 as grid container
+  $("body").append('<div id="grid1"></div>');
+  
+  
+  for(i=1;i<=nhght;i++) {
+          //div .gridrow is row container
+    $("#grid1").append('<div class="gridrow"></div>');
 
-for(i=1;i<=nhght;i++) {
-  //div .gridrow is row container
-  $("#grid1").append('<div class="gridrow"></div>');
+          //div .boxTp1 is each box in the row
+    for(j=1;j<=nwide;j++) {
+      $(".gridrow:last-child").
+      append("<div class='boxTp1'></div>"); 
+    } 
+  }
+         //fit boxes to screen
+  addboxcss();
+         //attach event listener to new grid 
+  $(".boxTp1").mouseover(() => colorMode(event))  ;
 
-  //div .boxTp1 is each box in the row
-  for(j=1;j<=nwide;j++) {
-    $(".gridrow:last-child").
-    append("<div class='boxTp1'>"+j+"</div>"); 
-  } 
+}        //end of function buildGrd
+
+function addboxcss() {
+       //calculate Max width height for screen size
+  pxwide = (.9*window.innerWidth / nwide -2) + 'px';
+  pxhght = (.9*window.innerHeight / nhght -2) + 'px';
+  
+  $(".boxTp1").css("width",  pxwide);
+  $(".boxTp1").css("height", pxhght);
 }
 
-//calculate Max width height for screen size
-pxwide = (.9*window.innerWidth / nwide -2) + 'px';
-pxhght = (.9*window.innerHeight / nhght -2) + 'px';
+//Function for changing box colors
+function colorMode(e) {
+  var str0, numStart, curropc, newopc, rgba;
 
-$(".boxTp1").css("width",  pxwide);
-$(".boxTp1").css("height", pxhght);
 
-//alert(pctwide + " " + window.innerHeight + " " + pcthght);
+  if (mode == 0) {
+    $(e.target).css("background-color", color1);}
+  else if (mode == 1) {
+    applyRandColor();
+    $(e.target).css("background-color", color1);}
+  else if (mode == 2) {
+    
+    str0 = $(e.target).css("background-color");
+    numStart = (str0.lastIndexOf(",") + 1);
+    curropc = (str0.slice(numStart));
+    newopc = parseFloat(curropc) + 0.2;
+ 
+    rgba = str0.slice(0, numStart) + newopc + ")";
+    $(e.target).css("background-color", rgba);   
+  }
 }
 
-//when clrbtn clicked, clear grid & buildGrid
-$("#clrbtn").submit( function() {
+
+
+//Event Listener: Make new grid
+$("#mkGrd").submit( function() {
     buildGrid($("#gWid").val(), $("#gHgt").val());
   }
 );
 
-//prevent reload page on form submit
-$("#clrbtn").submit(function(e) { 
-  e.preventDefault(); } );
+//Event Listener: Hover effect
+//$(".boxTp1").mouseover(() => colorMode(event))  ;
 
-
-//Hover effect
-
-$(".boxTp1").mouseover(
-  function(){
-    $(this).css("background-color", "yellow"); 
+//Event Listeners: color input, change label color
+$("#colorChoice").on("input", 
+  function() {
+    mode = 0;
+    color1 = $("#colorChoice").val();
+    $("#lblcolor").css("background-color", color1);  
+    $("#colorText").val(color1);
   }
 );
 
-/*
-$(".boxTp1").hover(
-  function(){
-    $(this).css("background-color", "yellow");},  
-  function(){
-    $(this).css("background-color", "yellow");}
+//Event listener: Rainbow Mode
+$("#rainbowbtn").on("click", () => applyRandColor());
+
+function applyRandColor()  {        
+    mode = 1;      
+    color1 = "#" + Math.floor(Math.random()*16777215).
+             toString(16);    // random color  
+    $("#lblcolor").css("background-color", color1);  
+    $("#colorText").val(color1); 
+}
+
+//Event Listener: Gradient Mode 
+$("#gradbtn").on("click", 
+  function() {
+    gradinit();
+    mode = 2;
+  }
 );
-*/
+
+function gradinit() { 
+  var str1;
+  
+  $(".boxTp1").each(
+    function() {
+      str1 = $(this).css("background-color");
+      $(this).css("background", 
+        convToRBGA(str1) );
+     // $("body").append(
+     //   $(this).css("background-color"));
+    } 
+  ); 
+}
+
+function convToRBGA(tmprgb)  {
+  var newrgb = tmprgb.slice(0,3) + "a" + tmprgb.slice(3,tmprgb.length-1) + "," + 0 + ")";
+  
+  return newrgb;
+}
+
+//prevent reload page on form submit
+$("#mkGrd").submit(function(e) { 
+  e.preventDefault(); } );
+
+//prevent enter key from submitting form on color input
+$(window).keydown(
+  function(event){ 
+    if(event.keyCode == 13 && 
+      $("#colorText").is(":focus")) { 
+        event.preventDefault(); 
+  
+        mode = 0;
+        $("#colorText").blur();
+        color1 = $("#colorText").val();
+        
+        $("#lblcolor").css("background-color", color1);  
+        $("#colorText").val(color1);
+        return false; 
+    }   
+  }
+);
